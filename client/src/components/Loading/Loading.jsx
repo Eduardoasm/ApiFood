ac24460c43af4ad4b50f647ee27bf314b8b9f05c
unknown <eduardoasm@gmail.com>
2022-08-14 11:52:52 -0400

    project

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import "./loading.css"
 export default function Loading(){
     return(
         <div className="loading">
-            <img className="gifLoad" src={loading} alt= "loading"/>
+            <img className="gifLoad" src={loading} alt= "loading" margin-top="600px" width="500px" height="500px"/>
         </div>
 
     )
